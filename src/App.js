3ea82d4731484694bf77b01bce1aaaa58499ffe8
Anil Choudhary <choudharya335@gmail.com>
2018-12-29 13:59:31 +0530

    jest added and prettir added to format code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React from 'react'
 import './styles.css'
 import { hot } from 'react-hot-loader'
+const Warning = React.lazy(() => import('./warning'))
 
 class App extends React.Component {
-    state = {
-        count: 0
-    }
+  state = {
+    count: 0
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>Hello World</h1>
-                <h2 className={this.state.count > 5 ? 'warning' : null}>Count: {this.state.count} </h2>
-                <button onClick={() => this.setState(state => ({ count: state.count + 1 }))}>+</button>
-                <button onClick={() => this.setState(state => ({ count: state.count - 1 }))}>-</button>
-            </div>
-        )
-    }
+  render() {
+    const { count } = this.state
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <h2 className={count > 5 ? 'warning' : null}>Count: {count} </h2>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
+        >
+          -
+        </button>
+        {count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
+      </div>
+    )
+  }
 }
 
-export default  hot(module)(App)
+export default hot(module)(App)
---
+++ src/warning.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const warning = () => <span className={'warning'}>Take it easy!</span>
+
+export default warning
