139ea3a98a2b0a2728708445fa7286445fdb947f
Anil Choudhary <choudharya335@gmail.com>
2018-12-29 14:56:09 +0530

    error boundary added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import '@babel/polyfill'
+import App from './App'
+import DefaultErrorBoundary from './DefaultErrorBoundary'
+import './styles.css'
 
-ReactDOM.render(<React.StrictMode>
-    <App />
-</React.StrictMode>, document.getElementById('app'))
+ReactDOM.render(
+  <React.StrictMode>
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
+  </React.StrictMode>,
+  document.getElementById('app')
+)
